0a3424383cfd83732ef6995e8b3cc76481e42fcb
cybercops <temmar.abdessamad@gmail.com>
2021-11-30 14:39:03 +0100

    lab 7

---
+++ lab7/spring-keycloak-authz-finale/src/main/java/com/example/authz/app/Lab7CompleteApplication.java
@@ -0,0 +1,12 @@
+package com.example.authz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7CompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7CompleteApplication.class, args);
+	}
+}
